6ce720907e85db187d36e1818f9699c7657c3227
Francesca <fra_9o@live.it>
2022-03-13 10:17:26 +0000

    Spacing classes: fixed names
    fixed some names that were still following the old logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
 console.log("Hi! Welcome to Francesca's Portfolio Site")
+let cardsUp = false;
+const process1El = document.querySelector("#process1");
+const processCardsEl = document.querySelector("#process-cards");
+
+process1El.addEventListener("click", function(){
+  console.log("button pressed");
+  if (cardsUp === false){
+    processCardsEl.classList.add("cards-up");
+    cardsUp = true;
+  }
+});
+
 
 function menuToggle () {
   var x = document.getElementById('myNavtoggle')
