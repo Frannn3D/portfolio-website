bf9f70fd592d3b5886844830e130d4ef70fbf616
Francesca <fra_9o@live.it>
2022-04-04 13:22:46 +0100

    scripts changes
    added functionalities for Quick & tasty page

---
+++ js/flex-cards.js
@@ -0,0 +1,34 @@
+// const card1El = document.querySelector("#card1");
+// const card2El = document.querySelector("#card2");
+
+// card2El.addEventListener("click", function () {
+//   card2El.classList.toggle("closed");
+//   card1El.classList.toggle("closed");
+// });
+
+const cardsArr = Array.from(document.querySelectorAll(".process-card"));
+let height1 = cardsArr[0].offsetHeight;
+console.log(height1);
+
+// for of.. height> height2? height = height2 : height
+
+// let i = 1;
+// for (height of cardsArr) {
+//   if (i === cardsArr.length) continue;
+//   // console.log(cardsArr[i].offsetHeight);
+//   let height2 = cardsArr[i].offsetHeight;
+//   height1 = height1 > height2 ? height1 : height2;
+//   console.log(height1);
+//   i++;
+// }
+
+cardsArr.forEach((card) => {
+  card.addEventListener("click", function () {
+    card.classList.toggle("closed");
+    let activeCard = cardsArr.find((card) => card.classList.contains("active"));
+    console.log(activeCard);
+    activeCard.classList.remove("active");
+    activeCard.classList.add("closed");
+    card.classList.add("active");
+  });
+});
--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,30 @@
-console.log("Hi! Welcome to Francesca's Portfolio Site")
-let cardsUp = false;
-const process1El = document.querySelector("#process1");
-const processCardsEl = document.querySelector("#process-cards");
+// console.log("Hi! Welcome to Francesca's Portfolio Site");
+// let cardsUp = false;
+// const process1El = document.querySelector("#process1");
+// const processCardsEl = document.querySelector(".process-cards");
+// const cardsEl = Array.from(document.querySelectorAll(".card"));
 
-process1El.addEventListener("click", function(){
-  console.log("button pressed");
-  if (cardsUp === false){
-    processCardsEl.classList.add("cards-up");
-    cardsUp = true;
-  }
-});
+// for (let i = 0; i < cardsEl.length; i++) {
+//   cardsEl[i].addEventListener("click", function(e){
+//     console.log("button pressed");
+//     if (cardsUp === false){
+//       processCardsEl.classList.add("active");
+//       cardsUp = true;
+//     }
+//     console.log(e);
+//     const cardPressed = e.path.find(item => item === "div#process");
+//     console.log(cardPressed);
+//   })
+// }
 
+// process1El.addEventListener("click", function(){
+// });
 
-function menuToggle () {
-  var x = document.getElementById('myNavtoggle')
-  if (x.className === 'navtoggle') {
-    x.className += ' responsive'
+function menuToggle() {
+  var x = document.getElementById("myNavtoggle");
+  if (x.className === "navtoggle") {
+    x.className += " responsive";
   } else {
-    x.className = 'navtoggle'
+    x.className = "navtoggle";
   }
 }
