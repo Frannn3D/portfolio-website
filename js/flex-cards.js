9fd8ff7cf925963a181c053fc3c21aa93c980a9d
Francesca <fra_9o@live.it>
2022-04-06 13:44:13 +0100

    Update flex-cards.js

--- js/flex-cards.js
+++ js/flex-cards.js
@@ -1,57 +1,53 @@
 // const card1El = document.querySelector("#card1");
 // const card2El = document.querySelector("#card2");
 
-// card2El.addEventListener("click", function () {
-//   card2El.classList.toggle("closed");
-//   card1El.classList.toggle("closed");
-// });
-
-const cardsArr = Array.from(document.querySelectorAll(".process-card"));
+const labelsArr = Array.from(document.querySelectorAll(".label"));
 // let height1 = cardsArr[0].offsetHeight;
-console.log(height1);
-
-// for of.. height> height2? height = height2 : height
-
-// let i = 1;
-// for (height of cardsArr) {
-//   if (i === cardsArr.length) continue;
-//   // console.log(cardsArr[i].offsetHeight);
-//   let height2 = cardsArr[i].offsetHeight;
-//   height1 = height1 > height2 ? height1 : height2;
-//   console.log(height1);
-//   i++;
-// }
+// console.log(height1);
 
 let touched = false;
 
-cardsArr.forEach((card) => {
-  card.addEventListener("click", function () {
-    // do something
-    card.classList.toggle("closed");
-    let activeCard = cardsArr.find((card) => card.classList.contains("active"));
-    console.log(activeCard);
-    activeCard.classList.remove("active");
-    activeCard.classList.add("closed");
-    card.classList.add("active");
+function cardsMovements(label) {
+  // Find the card number
+  const number = label.querySelector(".process-number").textContent;
+  console.log(number);
+  let activeCard = document.querySelector(`#card${number}`);
+  console.log(activeCard);
+
+  // find the previous active card and set it as closed
+  let oldActive = document.querySelector(".active");
+  console.log(oldActive);
+
+  /////// check it isn't the same card as before
+  if (oldActive !== activeCard) {
+    oldActive.classList.remove("active");
+    oldActive.classList.add("closed");
+
+    // set the new active card as active
+    activeCard.classList.remove("closed");
+    activeCard.classList.add("active");
+  } else {
+    // if the card is the same as the old one (third click on the same card)
+    //remove closed
+    activeCard.classList.toggle("closed");
+  }
+}
+
+labelsArr.forEach((label) => {
+  label.addEventListener("click", function (e) {
+    cardsMovements(label);
   });
 });
 
-cardsArr.forEach((card) => {
-  card.addEventListener("touchstart", function (e) {
+labelsArr.forEach((label) => {
+  label.addEventListener("touchstart", function (e) {
     e.preventDefault(); // without it, the desktop version will detect multiple clicks / touchstart, closing immediately after the card
     if (!touched) {
       touched = true;
-      card.classList.toggle("closed");
-      let activeCard = cardsArr.find((card) =>
-        card.classList.contains("active")
-      );
-      console.log(activeCard);
-      activeCard.classList.remove("active");
-      activeCard.classList.add("closed");
-      card.classList.add("active");
+      cardsMovements(label);
       setTimeout(function () {
         touched = false;
-      }, 500);
+      }, 400);
     }
   });
 });
