a45d740b14c1b3cd7415c430c63c65dea0bf59ff
Fran <fra_9o@live.it>
2022-06-11 15:03:46 +0100

    Adding Book-pop to the website

---
+++ js/book-pop.js
@@ -0,0 +1,14 @@
+const prevBtn = document.querySelector("#btn-prev");
+const nextBtn = document.querySelector("#btn-next");
+const wireframes = Array.from(document.querySelectorAll(".wireframe"));
+const dots = Array.from(document.querySelectorAll(".dot"));
+let indexShown = 0;
+const showD = true;
+
+prevBtn.addEventListener("click", function () {
+  previousPicture(wireframes, indexShown);
+});
+
+nextBtn.addEventListener("click", function () {
+  nextPicture(wireframes, indexShown, true, dots);
+});
